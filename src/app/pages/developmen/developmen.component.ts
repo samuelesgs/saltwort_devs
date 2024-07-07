66ed7da059757f405108f358147769a41f2cb452
Samuel <69209156+samuelesgs@users.noreply.github.com>
2024-07-06 19:14:53 -0700

    navbar inicial

---
+++ src/app/pages/developmen/developmen.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-developmen',
+  standalone: true,
+  imports: [],
+  templateUrl: './developmen.component.html',
+  styleUrl: './developmen.component.css'
+})
+export class DevelopmenComponent {
+
+}
