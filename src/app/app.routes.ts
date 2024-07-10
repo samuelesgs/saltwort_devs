34da40e33d4ec07e15d8455839cc9be9cc5e5109
pavi <106630766+LuisCalderonNava@users.noreply.github.com>
2024-07-09 22:06:40 -0700

    navbar e inicio
    nav bar arreglos
    inicio primera seccion
    creacion de componentes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { DevelopmenComponent } from './pages/developmen/developmen.component';
+import { AplicationsComponent } from './pages/aplications/aplications.component';
+import { AboutComponent } from './pages/about/about.component';
 
 export const routes: Routes = [
     {path : 'Home', component : HomeComponent},
     {path : 'Developers', component : DevelopmenComponent},
+    {path : 'Apps', component : AplicationsComponent},
+    {path : 'About_of', component : AboutComponent},
     {path: '**',  redirectTo: 'Home'},
 
 ];
---
+++ src/app/pages/aplications/aplications.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-aplications',
+  standalone: true,
+  imports: [],
+  templateUrl: './aplications.component.html',
+  styleUrl: './aplications.component.css'
+})
+export class AplicationsComponent {
+
+}
