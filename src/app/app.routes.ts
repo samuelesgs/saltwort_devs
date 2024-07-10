7882277d702f631f4cbe7fb68509319fd8b6632a
Samuel <69209156+samuelesgs@users.noreply.github.com>
2024-07-09 19:44:12 -0700

    Correccion navbar e incorporacion de footer y manejo de rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { DevelopmenComponent } from './pages/developmen/developmen.component';
 
 export const routes: Routes = [
     {path : 'Home', component : HomeComponent},
+    {path : 'Developers', component : DevelopmenComponent},
     {path: '**',  redirectTo: 'Home'},
 
 ];
