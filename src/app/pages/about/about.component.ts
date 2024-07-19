f0c59f91089e2dbc95cf04dc2e94e91a994311f7
Akemi Herrera <akselherrera18@gmail.com>
2024-07-18 19:09:49 -0700

    Sección de acerca de

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,12 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
+  public currentContainer = "";
 
+  public showContainer(current: string) {
+    this.currentContainer = current;
+  }
 }
