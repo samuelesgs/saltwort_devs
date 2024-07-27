8ee279ef9a7f47aebcfbec8a9a37e133f2bcce7a
Akemi Herrera <akselherrera18@gmail.com>
2024-07-26 21:24:15 -0700

    Mejora en la animación

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -18,31 +18,15 @@ export class AboutComponent {
 
   public showContainer(event: Event, current: string) {
     const container = event.target as HTMLElement;
-    const containerInfo = document.getElementById('container-info')!;
-    const rect = container.getBoundingClientRect();
-    const rectInfo = containerInfo.getBoundingClientRect();
-    container.style.backgroundColor = '#252525';
-    if(this.previusContainer) {
-      const containerBtn = document.getElementById('container-btn')!;
-      this.previusContainer.style.backgroundColor = 'transparent';
-      containerBtn.appendChild(this.previusContainer)
-      container.removeChild(this.previusContainer);
+    const containerBtn = document.getElementById('container-btn')!;
+    if(current == 'vission' || current == 'objectives') {
+      container.remove();
+      containerBtn.prepend(container);
     }
-    this.previusContainer = container;
-    containerInfo.appendChild(container);
-    containerInfo.animate([
-      {
-        transform: `translate(${rect.left - rectInfo.left}px, ${rect.top - rectInfo.top}px)`,
-        width: `${0}%}`
-      },
-      {
-        transform: 'translate(0px, 0px)',
-        width: '100%'
-      }
-    ], {
-      duration: 500,
-      easing: 'ease-in-out'
-    })
+    container.classList.add("box-mission");
+    setTimeout(() => {
+        container.classList.add('current-box')
+    }, 450);
     this.currentContainer = current;
   }
 }
