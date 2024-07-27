b896dedc2ff24fec46dc567cfa3316ccd1c59cee
Akemi Herrera <akselherrera18@gmail.com>
2024-07-26 20:29:46 -0700

    Correccion de diseño falta de animacion

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,3 +1,4 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
@@ -12,8 +13,36 @@ import { Component } from '@angular/core';
 })
 export class AboutComponent {
   public currentContainer = "";
+  private previusContainer: HTMLElement | null = null;
 
-  public showContainer(current: string) {
+
+  public showContainer(event: Event, current: string) {
+    const container = event.target as HTMLElement;
+    const containerInfo = document.getElementById('container-info')!;
+    const rect = container.getBoundingClientRect();
+    const rectInfo = containerInfo.getBoundingClientRect();
+    container.style.backgroundColor = '#252525';
+    if(this.previusContainer) {
+      const containerBtn = document.getElementById('container-btn')!;
+      this.previusContainer.style.backgroundColor = 'transparent';
+      containerBtn.appendChild(this.previusContainer)
+      container.removeChild(this.previusContainer);
+    }
+    this.previusContainer = container;
+    containerInfo.appendChild(container);
+    containerInfo.animate([
+      {
+        transform: `translate(${rect.left - rectInfo.left}px, ${rect.top - rectInfo.top}px)`,
+        width: `${0}%}`
+      },
+      {
+        transform: 'translate(0px, 0px)',
+        width: '100%'
+      }
+    ], {
+      duration: 500,
+      easing: 'ease-in-out'
+    })
     this.currentContainer = current;
   }
 }
