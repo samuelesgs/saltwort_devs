dc0d480233d238b8f5a83ba84c9b6fbb063af542
Samuel <69209156+samuelesgs@users.noreply.github.com>
2026-01-10 21:26:36 -0700

    react ts

---
+++ react_app/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>React + TypeScript 🚀</h1>
+      <p>Proyecto base SIN Vite.</p>
+    </div>
+  );
+}
+
+export default App;
