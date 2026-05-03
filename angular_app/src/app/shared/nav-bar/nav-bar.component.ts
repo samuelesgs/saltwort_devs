91f2ec956d4e916fcd136a58436d20cab847e8c8
akselherreraEpam <aksel_herrera@epamneoris.com>
2026-05-03 12:41:05 -0700

    Cambios diseño: Navbar

--- angular_app/src/app/shared/nav-bar/nav-bar.component.ts
+++ angular_app/src/app/shared/nav-bar/nav-bar.component.ts
@@ -3,27 +3,40 @@ import { RouterModule } from '@angular/router';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { LocalStorageManager } from '../../utils/localStorageManager';
 import { AppService } from '../../app.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
-    selector: 'app-nav-bar',
-    imports: [
-        RouterModule,
-        TranslateModule,
-    ],
-    templateUrl: './nav-bar.component.html',
-    styleUrl: './nav-bar.component.css'
+  selector: 'app-nav-bar',
+  imports: [
+    CommonModule,
+    RouterModule,
+    TranslateModule,
+  ],
+  templateUrl: './nav-bar.component.html',
+  styleUrl: './nav-bar.component.css'
 })
 export class NavBarComponent {
 
-  currentLang : string = "es";
+  currentLang: string = "es";
   LocalStorageManager = new LocalStorageManager();
+  isOpen = false;
 
-  constructor(private translateService : TranslateService, private appService : AppService) {
-    this.translateService.use(this.LocalStorageManager.getItem("lang"));    
+  constructor(private translateService: TranslateService, private appService: AppService) {
+    this.translateService.use(this.LocalStorageManager.getItem("lang"));
   }
 
-  setLang(lang : string) {
-    this.LocalStorageManager.setItem("lang" , lang);
+
+  toggleDropdown() {
+    this.isOpen = !this.isOpen;
+  }
+
+  selectLang(lang: string) {
+    this.setLang(lang); // ya existente
+    this.isOpen = false;
+  }
+
+  setLang(lang: string) {
+    this.LocalStorageManager.setItem("lang", lang);
     this.currentLang = lang;
     this.appService.setChangeLang(lang);
   }
